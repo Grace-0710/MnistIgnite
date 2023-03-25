3e912f80316e8f657463469e4dcdecf1a3ad322d
정승연 <sg04096@gmail.com>
2023-03-26 02:36:42 +0900

    train, trainer 주석 추가

--- train.py
+++ train.py
@@ -10,15 +10,15 @@ from data_loader import get_loaders
 
 def define_argparser():
     p = argparse.ArgumentParser()
-
-    p.add_argument('--model_fn', required=True)
+    # ArgumentParser : 프로그램을 실행시에 커맨드 라인에 인수를 받아 처리를 간단히 할 수 있도록 하는 표준 라이브러리이다.
+    p.add_argument('--model_fn', required=True) # 무조건 필요
     p.add_argument('--gpu_id', type=int, default=0 if torch.cuda.is_available() else -1)
 
     p.add_argument('--train_ratio', type=float, default=.8)
 
     p.add_argument('--batch_size', type=int, default=256)
     p.add_argument('--n_epochs', type=int, default=20)
-    p.add_argument('--verbose', type=int, default=2)
+    p.add_argument('--verbose', type=int, default=2) # 얼마나 자주 출력을 할까?
 
     config = p.parse_args()
 
@@ -35,13 +35,18 @@ def main(config):
     print("Valid:", len(valid_loader.dataset))
     print("Test:", len(test_loader.dataset))
 
-    model = ImageClassifier(28**2, 10).to(device)
+    model = ImageClassifier(28**2, 10).to(device) # 784개의 데잍를 10개의 class로 분류
     optimizer = optim.Adam(model.parameters())
-    crit = nn.NLLLoss()
+    crit = nn.CrossEntropyLoss()
+    # CrossEntropyLoss : Classification주 사용, 정답 클래스에 해당하는 스코어에 대해서만 로그합을 구하여 최종 Loss
+    # 위 모델 softmax로 끝나있음
+    # 1. Softmax 함수를 통해 이 값들의 범위는 [0,1], 총 합은 1
+    # 2. 1-hot Label (정답 라벨)과의 Cross Entropy를 통해 Loss
+   
 
-    trainer = Trainer(config)
+    trainer = Trainer(config) # ignite 사용항 구현
     trainer.train(model, crit, optimizer, train_loader, valid_loader)
 
 if __name__ == '__main__':
     config = define_argparser()
-    main(config)
+    main(config) # main에서 config를 받아!
--- trainer.py
+++ trainer.py
@@ -19,8 +19,9 @@ VERBOSE_EPOCH_WISE = 1
 VERBOSE_BATCH_WISE = 2
 
 
-class MyEngine(Engine):
-
+class MyEngine(Engine): # 기본 Engine상속받아 custom
+    # 기본 param : func == process_function
+    # crit, optimizer, config : 추가
     def __init__(self, func, model, crit, optimizer, config):
         # Ignite Engine does not have objects in below lines.
         # Thus, we assign class variables to access these object, during the procedure.
@@ -36,31 +37,37 @@ class MyEngine(Engine):
 
         self.device = next(model.parameters()).device
 
-    @staticmethod
+    @staticmethod # 정적 메서드 : self지정X, 인스턴스의 상태를 변화시키지 않는 메서드를 만들 때 사용
     def train(engine, mini_batch):
-        # You have to reset the gradients of all model parameters
-        # before to take another step in gradient descent.
-        engine.model.train() # Because we assign model as class variable, we can easily access to it.
-        engine.optimizer.zero_grad()
+        engine.model.train()
+        engine.optimizer.zero_grad() # 한번의 iteration마다 불러오는거라 수행
 
-        x, y = mini_batch
+        x, y = mini_batch # data_loader.py __getitem__ (형태 : 튜플)
         x, y = x.to(engine.device), y.to(engine.device)
 
-        # Take feed-forward
-        y_hat = engine.model(x)
+        # Take feed-forward -> forward : 입력 텐서로부터 출력 텐서를 계산
+        y_hat = engine.model(x) # |x| = (batch size,784)
+        #|y|^ = (batch size,10) -> 10차원의 vector : 각 차원의 클래스별 확률값
 
         loss = engine.crit(y_hat, y)
         loss.backward()
-
-        # Calculate accuracy only if 'y' is LongTensor,
-        # which means that 'y' is one-hot representation.
+        # backward : 어떤 스칼라 값에 대한 출력 텐서의 변화도(gradient)를 전달받고, 동일한 스칼라 값에 대한 입력 텐서의 변화도를 계산
+        # loss.backward() : 전체 그래프는 손실에 대해 미분되며, 그래프 내의 모든 변수는 변화도가 누적된 .grad 함수를 갖게 됩니다.
+       
         if isinstance(y, torch.LongTensor) or isinstance(y, torch.cuda.LongTensor):
             accuracy = (torch.argmax(y_hat, dim=-1) == y).sum() / float(y.size(0))
+            # argmax : f(x)의 최대값
+            # (torch.argmax(y_hat, dim=-1) == y).sum() : 값이 y인값들의 총 합
+            # float(y.size(0)) : batch size
         else:
             accuracy = 0
 
+        # < 학습 보조 지표 >
         p_norm = float(get_parameter_norm(engine.model.parameters()))
+        # p_norm 커질수록 파라미터 애들이 많아져 -> 수행중
         g_norm = float(get_grad_norm(engine.model.parameters()))
+        # gradient : 학습초기때는 커, 학습이 진행됨에 따라 줄어들어
+        # 학습이 진행되도 값이 커지면 불안정하다는 증거 == NaN
 
         # Take a step of gradient descent.
         engine.optimizer.step()
@@ -95,6 +102,7 @@ class MyEngine(Engine):
         }
 
     @staticmethod
+    # 학습이 진행되는 과정을 이쁘게 표시하고 싶어요!
     def attach(train_engine, validation_engine, verbose=VERBOSE_BATCH_WISE):
         # Attaching would be repaeted for serveral metrics.
         # Thus, we can reduce the repeated codes by using this function.
@@ -118,6 +126,7 @@ class MyEngine(Engine):
         # If the verbosity is set, statistics would be shown after each epoch.
         if verbose >= VERBOSE_EPOCH_WISE:
             @train_engine.on(Events.EPOCH_COMPLETED)
+            #EPOCH이 끝났을떄 ( 완료 ) print!
             def print_train_logs(engine):
                 print('Epoch {} - |param|={:.2e} |g_param|={:.2e} loss={:.4e} accuracy={:.4f}'.format(
                     engine.state.epoch,
@@ -174,6 +183,8 @@ class Trainer():
         model, crit, optimizer,
         train_loader, valid_loader
     ):
+        # engine 생성하여 실행
+        # iteration이 돌떄마다 callback 함수 실행
         train_engine = MyEngine(
             MyEngine.train,
             model, crit, optimizer, self.config
